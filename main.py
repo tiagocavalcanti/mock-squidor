2834ac108d1cd98ec424c1ef680ba141d014abb0
Tiago Cavalcanti <tiago85@gmail.com>
2023-05-08 18:42:35 -0300

    Commit Inicial

---
+++ main.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, Response
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return '''  <b>[GET]</b> <a href="/portador"> /portador </a> &emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Lista todos os portadores<br>
+                <b>[GET]</b> <a href="/fatura"> /fatura </a> &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Lista a fatura de todos os portadores <br>
+                <b>[GET]</b> <a href="/fatura?idPortador="> /fatura?idPortador= </a> &emsp;&ensp;&nbsp; Lista as faturas do portador <br>
+                <b>[POST]</b> <a href="/cobranca/{idFatura}="> /cobranca/{idFatura}</a> &ensp;&nbsp; Envia uma fatura para cobrança <br>
+    '''
+
+# Portador
+@app.route("/portador")
+def portador_list_all():
+    return Response(open("portador.json"), mimetype="application/json")
+
+
+# Fatura
+@app.route("/fatura")
+def fatura_list():
+    with open("fatura.json") as jsonfile:
+        data = json.load(jsonfile)
+        # lista as Faturas por Portador
+        if "idPortador" in request.args:
+            select = [x for x in data if str(x["idPortador"]) == request.args["idPortador"]]
+            return Response(json.dumps(select), mimetype="application/json")
+
+        # lista todas
+        else:
+            return data
+
+
+# Cobrança
+@app.route("/cobranca/<idFatura>", methods=['POST'])
+def cobranca_send(idFatura):
+
+    with open("fatura.json") as jsonfile:
+        data = json.load(jsonfile)
+        fatura = [item for item in data if str(item["id"]) == str(idFatura)]
+        if fatura:
+            return fatura[0]
+        else:
+            return "{}"
